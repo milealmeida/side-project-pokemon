854bb251191ef0bda9558dd985ba1e026c458720
Milena Almeida <milenaalmeidadev@hotmail.com>
2022-12-11 09:48:44 -0400

    config eslint, prettier, husky, styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyle from '../styles/global';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Head>
+        <title>Pokémon Web</title>
+        <meta name="description" content="Side project based on Pokémons" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,3 @@
-import Head from 'next/head';
-
-import 'index.css';
-
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Pokémon Web</title>
-        <meta name="description" content="Side project based on Pokémons" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Hello world
-    </div>
-  );
+  return <div>Hello world</div>;
 }
