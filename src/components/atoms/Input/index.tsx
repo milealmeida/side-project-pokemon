0cca8b3ba67361e9b256708f358f35c9fb9e1209
Milena Almeida <milenaalmeidadev@hotmail.com>
2023-01-31 20:52:24 -0400

    feat: create request by name

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,10 +1,56 @@
+import { ChangeEvent, useState } from 'react';
 import Image from 'next/image';
+import { gql } from '@apollo/client';
+import { createApolloClient } from 'graphql/apollo-client';
+
 import { Container, Input, Wrapper } from './styles';
 
+export const GET_POKEMONS = gql`
+  query pokeAPIquery($name: String!) {
+    pokemon_v2_pokemon(where: { name: { _like: $name } }) {
+      name
+      id
+      weight
+      height
+      pokemon_v2_pokemontypes {
+        pokemon_v2_type {
+          name
+          id
+        }
+      }
+    }
+  }
+`;
+
 export function InputComponent() {
+  const [value, setValue] = useState<string>();
+
+  const apolloClient = createApolloClient();
+
+  async function handlePokemonName(name: string) {
+    const response = await apolloClient.query({
+      query: GET_POKEMONS,
+      variables: {
+        name: `%${name}%`,
+      },
+    });
+
+    console.log(response);
+  }
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+
+    setValue(newValue);
+  };
+
+  if (value && value?.length > 2) {
+    handlePokemonName(value);
+  }
+
   return (
     <Wrapper>
-      <Input placeholder="Pesquisar Pokémon" type="text" />
+      <Input placeholder="Pesquisar Pokémon" type="text" onChange={onChange} />
       <Container>
         <Image
           src="/img/svg/search.svg"
