67175e2a8556a0cc796eb4a2d8d48d68caa33571
Milena Almeida <milenaalmeidadev@hotmail.com>
2023-02-14 20:32:27 -0400

    feat(modal): create pokemon infos

--- src/components/organisms/Modal/index.tsx
+++ src/components/organisms/Modal/index.tsx
@@ -1,11 +1,82 @@
+import { Type } from 'components/atoms/Type';
+import Image from 'next/image';
 import { Container, Wrapper } from './styles';
 
 export function Modal() {
   return (
     <Wrapper>
       <Container>
-        <p>Modal</p>
-        <button type="button">Sair</button>
+        <div>
+          <Image
+            src="/img/charizard.png"
+            alt="Charizard"
+            width={200}
+            height={400}
+          />
+
+          <span>#006</span>
+          <h1>Charizard</h1>
+
+          <div>
+            <Type type="flying" />
+            <Type type="fire" />
+          </div>
+
+          <div>
+            <Image
+              src="/img/svg/weight.svg"
+              alt="Balança"
+              width={10}
+              height={10}
+            />
+            <div>90.5 kg</div>
+            <span>Peso</span>
+          </div>
+
+          <div>
+            <Image
+              src="/img/svg/height.svg"
+              alt="Balança"
+              width={10}
+              height={10}
+            />
+            <div>1.7 m</div>
+            <span>Altura</span>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h2>Stats</h2>
+          </div>
+
+          <div>
+            <div>
+              <span>HP</span>
+              <strong>78</strong>
+            </div>
+            <div>
+              <span>Attack</span>
+              <strong>84</strong>
+            </div>
+            <div>
+              <span>Defense</span>
+              <strong>78</strong>
+            </div>
+            <div>
+              <span>Sp. Atk</span>
+              <strong>109</strong>
+            </div>
+            <div>
+              <span>Sp. Def</span>
+              <strong>85</strong>
+            </div>
+            <div>
+              <span>Speed</span>
+              <strong>100</strong>
+            </div>
+          </div>
+        </div>
       </Container>
     </Wrapper>
   );
--- src/components/organisms/Modal/styles.ts
+++ src/components/organisms/Modal/styles.ts
@@ -4,17 +4,22 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  position: absolute;
+  position: fixed;
   top: 0;
+  left: 0;
   z-index: 10;
-  background-color: pink;
+  background-color: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100vh;
-  padding: 3.4rem;
+  padding: 3.6rem;
 `;
 
 export const Container = styled.div`
-  background-color: white;
-  max-width: 80rem;
+  display: flex;
+  gap: 4rem;
+  background-color: rgba(0, 0, 0, 0.5);
+  max-width: 95rem;
   width: 100%;
+  border-radius: 1.6rem;
+  color: white;
 `;
